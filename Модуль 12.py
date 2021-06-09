964c259e5dbc7ca0d366657e88b6de2206767741
KrylovMikhail <84391735+KrylovMikhail@users.noreply.github.com>
2021-06-09 16:04:57 +0200

    Add files via upload

---
+++ Модуль 12.py
@@ -0,0 +1,9 @@
+
+
+money = float(input('Введите сумму депозита: '))
+
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+
+deposit = list(map(round, [i * money / 100 for i in per_cent.values()]))
+print(deposit)
+print('Максимальная сумма, которую вы можете заработать — ', max(deposit))
